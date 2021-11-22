35bd0765b4adea0199112431e66cf344560d4dda
Shadowshay <ruijie27@gmail.com>
2021-11-22 23:44:39 +0800

    Update Code.cs

--- Assets/Scripts/Code.cs
+++ Assets/Scripts/Code.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Code : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class Code : MonoBehaviour
     public int netWorth = 1000; 
     public int daysPassed = 0;
     public TMPro.TextMeshPro Date ;
+    public Text cashText;
+    public int unitsOwned = 0;
 
     // number of unit of stocks for each stock pattern
     public int goodStock = 0;
@@ -17,9 +20,33 @@ public class Code : MonoBehaviour
     public int averageStock = 0;
 
     // value of stock 
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public float goodStockValue = 0;
+    public float badStockValue = 0;
+    public float averageStockValue = 0;
+=======
     public int goodStockValue = 0;
     public int badStockValue = 0;
     public int averageStockValue = 0;
+>>>>>>> parent of af54c8c (Analytics Table)
+=======
+    public int goodStockValue = 0;
+    public int badStockValue = 0;
+    public int averageStockValue = 0;
+>>>>>>> parent of af54c8c (Analytics Table)
+=======
+    public int goodStockValue = 0;
+    public int badStockValue = 0;
+    public int averageStockValue = 0;
+>>>>>>> parent of af54c8c (Analytics Table)
+=======
+    public int goodStockValue = 0;
+    public int badStockValue = 0;
+    public int averageStockValue = 0;
+>>>>>>> parent of af54c8c (Analytics Table)
 
     // singleton that other classes can take variables from
     private void Awake()
@@ -87,9 +114,11 @@ public class Code : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    cashText.text = "Cash: " + cash;
+    //}
+
 }
