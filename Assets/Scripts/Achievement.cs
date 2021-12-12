6a143212c297f3605c2303f7f4ecf6e4c04e1314
anon-is-anonymus <cylee301203@gmail.com>
2021-12-12 16:29:46 +0800

    Achievement Script + gameobject fix

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievement : MonoBehaviour
+{
+	public static Achievement Instance { get; private set; }
+
+	public bool winGame;
+	public bool Stonks;
+	public bool loseGame;
+	public bool speedRunner;
+
+	// singleton that other classes can take variables from
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/Notifications.cs
+++ Assets/Scripts/Notifications.cs
@@ -6,50 +6,60 @@ using UnityEngine;
 public class Notifications : MonoBehaviour
 {
 
-    public static Notifications Instance { get; private set; }
-    public GameObject notifScreen;
+	public static Notifications Instance { get; private set; }
+	public GameObject notifScreen;
+	public GameObject achievementScreen;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        notifScreen.SetActive(false);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		notifScreen.SetActive(false);
+		achievementScreen.SetActive(false);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
 
-    public void OpenNotif()
-    {
-        if (RandomEvents.Instance.returnValue != 0)
-        {
-            notifScreen.SetActive(true);
-            notifScreen.gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().SetText(RandomEvents.Instance.eventDisc);
-        }
-        else
-        {
-            notifScreen.gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().SetText("");
-            notifScreen.SetActive(false);
-        }
-    }
+	public void CheckAchievements()
+	{
+		if (Achievement.Instance.winGame == true || Achievement.Instance.Stonks == true || Achievement.Instance.loseGame == true || Achievement.Instance.speedRunner == true)
+		{
+			achievementScreen.SetActive(true);
+		}
+	}
 
-    public void CloseNotif()
-    {
-        notifScreen.SetActive(false);
-    }
+	public void OpenNotif()
+	{
+		if (RandomEvents.Instance.returnValue != 0)
+		{
+			notifScreen.SetActive(true);
+			notifScreen.gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().SetText(RandomEvents.Instance.eventDisc);
+		}
+		else
+		{
+			notifScreen.gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().SetText("");
+			notifScreen.SetActive(false);
+		}
+	}
+
+	public void CloseNotif()
+	{
+		notifScreen.SetActive(false);
+	}
 }
